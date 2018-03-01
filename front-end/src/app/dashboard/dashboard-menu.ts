c381da0c448dcdeadf2498d5534f26d0738dd434
Engy Fawaz <engy.fawaz@student.guc.edu.eg>
2018-03-01 17:36:58 +0200

    update workplace

--- front-end/src/app/dashboard/dashboard-menu.ts
+++ front-end/src/app/dashboard/dashboard-menu.ts
@@ -1,11 +1,14 @@
 import { NbMenuItem } from '@nebular/theme';
+import { StoreComponent } from './store/store.component';
 
 export const MENU_ITEMS: NbMenuItem[] = [
 
    { title: 'Dashboard', icon: 'nb-paper-plane', link: '/dashboard', home: true },
   {
     title: 'Store',
-    icon: 'fa fa-shopping-basket',
+
+    icon: '  fa fa-shopping-bag	  ',
+    
     link: '/dashboard/store',
     children: [
       {
@@ -13,14 +16,31 @@ export const MENU_ITEMS: NbMenuItem[] = [
         icon: 'fa fa-gamepad',
         link: '/dashboard/store/toystore',
 
-      }
-
-
+      }, 
   ]
+  },
+  {
+    title: 'My Items',
+    icon: 'fa fa-shopping-basket	',
+    link: '/dashboard/myitems'
+
   },
   {
     title: 'Cart',
     icon: 'fa fa-cart-arrow-down',
     link: '/dashboard/items'
+  },
+  {
+    title: 'Auth',
+    icon: 'fa fa-lock	',
+    children: [{
+      title: 'Login',
+      icon: 'fa fa-sign-in',
+      link: '/dashboard/authe/login',
+    },{
+      title: 'Sign up',
+      icon: 'fa fa-user-plus',
+      link: '/dashboard/authe/signup',
+    }]
   }
 ];
