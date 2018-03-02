198143c004a8a2194ac20e2c21e8c52da23fd817
Engy Fawaz <engy.fawaz@student.guc.edu.eg>
2018-03-02 18:09:40 +0200

    login and sign up working

--- back-end/api/controllers/authentication.js
+++ back-end/api/controllers/authentication.js
@@ -1,60 +1,92 @@
+//import { link } from 'fs';
 
-//const users = require('../models/users');
+table = require('../models/users');
 var mongoose = require('mongoose'),
-  moment = require('moment'),
-  Validations = require('../utils/Validations'),
-  hoba = mongoose.model('users'); 
-
+    moment = require('moment'),
+    Validations = require('../utils/Validations'),
+    hoba = mongoose.model('users');
+//hoba2 = mongoose.model('users');  
 
 module.exports=(router) =>{
-    
-    router.post('users/register' , (req , res)=>{
-        if(!req.body.email){
+    router.post('/login' , (req , res)=>{
+
+        let users = new hoba({
+            email:req.body.email.toLowerCase(),
+            //fullname:req.body.fullname.toLowerCase(),
+            password:req.body.password
+        });
+    hoba.findOne({email : req.body.email , password :req.body.password} ,function(err , users)   {
+        if(err){
+            //console.log(err);
+            //console.log('1');
+            res.send('aaa');
+            //return res.status(500).send();
+
+        }
+        if(!users){
+            //return res.status(404).send();
+            //console.log('1');
+            res.send('Wrong password');
+        }
+        else{
+            //res.render("http://localhost:4200/#/dashboard");
+            console.log('adasd');
+
             res.json({
-                success:false , message: 'check your Email'
+
+                success:false , message: 'doneeee'
+
+
             });
         }
-        else {
-            if(!req.body.name){
-                res.json({
-                    success:false , message: 'check your Name'
-                    
-                });
-            }
-            else{
-                if(!req.body.password){
-                    res.json({
-                        success:false , message: 'your password is wrong'
-                    });
-                }
-                else{
-                    
-                    res.send('Sign up successfully')
+        //res.send('ccc');
+        return res.status(200).send();
 
-                }
-                
-            }
-            
-        }
+
+    })
+
+
+
+});
+
+
+    router.post('/register' , (req , res)=>{
+
+
         let users = new hoba({
             email:req.body.email.toLowerCase(),
             fullname:req.body.fullname.toLowerCase(),
             password:req.body.password
         });
-        users.save(hoba.body);
-       users.save((err)=>{
-          //  if(err){
-              //  res.json({
-                //    success:false , message: 'not save ',err
-              //  });
-          //  }
-          //  else{
-               // res.json({
-                   // success:false , message: 'saved '
-               // });
-            //}
-        })
+
+    //users.save((hoba.body));
+
+    users.save((err)=>{
+        if(err){
+            res.send('not Saved');
+
+        }
+        else{
+            users.save((hoba.body));
+    res.json({
+        success:false , message: 'saved '
+    });
+}
+})
+});
+
+    return router;
+}
+/*module.exports=(router) =>{
+    
+    router.get('/login' , (req , res)=>{
+        res.json({
+             success:false , message: 'here '
+         });
+         
     });
      
     return router;
-}
+}*/
+
+       
--- back-end/api/models/users.js
+++ back-end/api/models/users.js
@@ -2,26 +2,26 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 var UsersSchema = mongoose.Schema({
-    name: {
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase:true
+    },
+    email: {
         type: String,
         required: true,
         unique: true,
         lowercase:true
-      },
-      
-  password: {
-    type: String,
-    required: true,
-    //unique:false
-    
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase:true
-  }
-  //updatedAt: Date
+    },
+    password: {
+        type: String,
+        required: true,
+        trim:true
+
+    },
+
+    //updatedAt: Date
 });
 
 mongoose.model('users', UsersSchema);
